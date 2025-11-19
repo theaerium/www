995bb0fda5efb1805d3475654a4a57cf4013a942
mdawess <michaelanthony.dawes@mail.utoronto.ca>
2025-11-18 20:08:25 -0800

    Update tags to add Apollo monitoring

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Analytics } from "@vercel/analytics/next";
+import Script from "next/script";
 
 const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
@@ -61,6 +62,18 @@ export default function RootLayout({
       <body className={`${geistMono.variable} antialiased overflow-x-hidden`}>
         {children}
         <Analytics />
+        <Script
+          id="apollo-tracking"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              function initApollo(){var n=Math.random().toString(36).substring(7),o=document.createElement("script");
+              o.src="https://assets.apollo.io/micro/website-tracker/tracker.iife.js?nocache="+n,o.async=!0,o.defer=!0,
+              o.onload=function(){window.trackingFunctions.onLoad({appId:"69166b03ca04170021c99b53"})},
+              document.head.appendChild(o)}initApollo();
+            `,
+          }}
+        />
       </body>
     </html>
   );
